a5ecfe386d67d0a0c555cd51f7f3f604dc2de55a
Ben P <przybyszewski.benjamin@gmail.com>
2022-02-10 12:46:18 -0600

    Test clone constructor

--- src/TestCases.java
+++ src/TestCases.java
@@ -6,9 +6,14 @@ public class TestCases {
     public static void main(String[] args) {
         Deck d = new Deck(52);
 
-        System.out.println(d.drawTopCard());
+        // System.out.println(d.drawTopCard());
         System.out.println();
-        System.out.println(d);
+        // System.out.println(d);
+
+        Card c = new Card('s',2);
+        System.out.println(c);
+        Card b = new Card(c);
+        System.out.println(b);
     }
     
 }
